d96bcb8d4c41a5cf31049dada1a330a930aca63b
Rini020 <rini.s@codaemonsoftwares.com>
2026-08-05 10:30:12 +0530

    move zero is in concpt building

--- #3.Arrays-EasyMedium/move-zeros.js
+++ #3.Arrays-EasyMedium/move-zeros.js
@@ -1,3 +1,34 @@
 /**
  * Move zeros : https://leetcode.com/problems/move-zeroes/
  */
+
+function moveZeros(nums){
+    if (nums.lngth <= 1) return;
+    let indexOfZeroElement = null;
+    for(let i = 0; i < nums.length; i++){
+        if (nums[i] == 0){
+            if(indexOfZeroElement == null){
+                indexOfZeroElement = i;
+            }
+        }else{
+            if(indexOfZeroElement == null) continue;
+            nums[indexOfZeroElement] = nums[i];
+            nums[i] = 0;
+            // if(nums[i - 1] == 0){
+            //     indexOfZeroElement = i - 1;
+            // }else{
+            //     indexOfZeroElement = i;
+            // }
+            while (indexOfZeroElement < i){
+                indexOfZeroElement++;
+                if (nums[indexOfZeroElement] == 0) break;
+            }
+        }
+    }
+    console.log(nums);
+}
+
+moveZeros([4, 2, 4, 0, 0, 3, 0, 5, 1, 0]);
+
+
+//17:11
