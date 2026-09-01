f393cd66db379588255386639dae92968bb2cb7c
Rini020 <rini.s@codaemonsoftwares.com>
2026-09-01 09:37:38 +0530

    solv missing numb

--- #3.Arrays-EasyMedium/missingNumber.js
+++ #3.Arrays-EasyMedium/missingNumber.js
@@ -20,6 +20,15 @@
 
 //10:38
 
+function returnMissingNumbs(nums) {
+    let sum = (nums.length * (nums.length + 1)) / 2;
+    let arraySum = 0;
+    for (let i = 0; i < nums.length; i++){
+        arraySum += nums[i];
+    }
+    return sum - arraySum;
+}
+
 //console.log(returnMissingNumbs([3, 0, 1]));
 //console.log(returnMissingNumbs([0, 1]));
 console.log(returnMissingNumbs([9, 6, 4, 2, 3, 5, 7, 0, 1]));
