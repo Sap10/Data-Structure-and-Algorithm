7e10665892b9902f9136378b9d7c9a61b972a34b
Rini020 <rini.s@codaemonsoftwares.com>
2026-09-22 10:35:03 +0530

    recursion

--- #3.Arrays-EasyMedium/single-number.js
+++ #3.Arrays-EasyMedium/single-number.js
@@ -1,20 +1,45 @@
 /**
  * https://leetcode.com/problems/single-number/
  * 
-<<<<<<< HEAD
- * @param {Array[] allNumbs}
+ * @param {Array[] nums}
  * @return number
  * 
  */
-
-function findSingleNumb(allNumbs){
+//Solution : 1
+function findSingleNumb(nums){
     let hashMap = {};
-    for(let i = 0; i < allNumbs.length; i++){
+    for(let i = 0; i < nums.length; i++){
+        if(!hashMap[nums[i]]){
+            hashMap[nums[i]] = 1;
+        }else{
+            hashMap[nums[i]] ++;
+        }
+    }
 
+    for(let i = 0; i < nums.length; i++){
+        if(hashMap[nums[i]] < 2){
+            return nums[i];
+        }
     }
 }
-=======
- * 
- * 
- */
->>>>>>> 382f6131352980cb9cd394bb2cb394720017f3df
+
+//console.log(findSingleNumb([2, 2, 1]));
+//console.log(findSingleNumb([1]));
+//console.log(findSingleNumb([4, 1, 2, 1, 2]));
+
+//Solution : 2
+
+//XOR gives 1 when the two values are different.
+//XOR gives 0 when the two values are the same.
+
+function findSingleNumb(nums){
+    let xor = 0;
+    for (let i = 0; i < nums.length; i++){
+        xor = xor ^ nums[i];
+    }
+    return xor;
+}
+
+console.log(findSingleNumb([2, 2, 1]));
+console.log(findSingleNumb([1]));
+console.log(findSingleNumb([4, 1, 2, 1, 2]));
---
+++ #4.Recursion/README.md
@@ -0,0 +1,21 @@
+# Recursion
+
+**Function call itself inside the function**
+
+**Function call itself to solve smaller version of the same problem**
+
+# Two parts of recursion :-
+
+**Base case :-** *Stop condition (when to stop calling itself)*
+
+**Recursive case :-** *Part where function call itself.*
+
+# Real life example :-
+
+**#1. Queue of people.**
+
+**#2. Comment Threads.**
+
+**#3. Organisational Hierarchis**
+
+6:29
